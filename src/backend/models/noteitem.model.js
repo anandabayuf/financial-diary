b2b6ebb3cc51df13dd4978566d2662535b874c28
Ananda Bayu <anandabayu090@gmail.com>
2024-03-20 10:00:04 +0700

    feat(backend): remove date validation

--- src/backend/models/noteitem.model.js
+++ src/backend/models/noteitem.model.js
@@ -5,31 +5,6 @@ const categoryNoteModel = require("./categorynote.model");
 const { ITEM_TYPE } = require("../constants/enum");
 const message = require("../constants/message");
 
-exports.isCanAdd = (note, data) => {
-	const noteYear = new Date(note.date).getFullYear();
-	const dataYear = new Date(data.date).getFullYear();
-	const noteMonth = new Date(note.date).getMonth() + 1;
-	const dataMonth = new Date(data.date).getMonth() + 1;
-	const dataDate = new Date(data.date).getDate();
-	const isCanAdd =
-		(dataYear + 1 === noteYear &&
-			dataMonth === 12 &&
-			dataDate >= 26 &&
-			dataDate <= 31) ||
-		(noteYear === dataYear &&
-			noteMonth === dataMonth &&
-			dataDate <= 25 &&
-			dataDate >= 1) ||
-		(noteYear === dataYear &&
-			dataMonth + 1 === noteMonth &&
-			dataDate >= 26 &&
-			(dataDate <= 31 ||
-				dataDate <= 30 ||
-				dataDate <= 29 ||
-				dataDate <= 28));
-	return isCanAdd;
-};
-
 //debit !== 0 && credit == 0
 //walletId !== undefined && categoryId == undefined
 exports.incomeTransaction = (walletNoteId, income) => {
@@ -97,106 +72,92 @@ exports.create = (noteId, data) => {
 		noteModel
 			.getById(noteId)
 			.then(async (note) => {
-				// const noteYear = new Date(note.date).getFullYear();
-				// const noteMonth = new Date(note.date).getMonth() + 1;
+				let payload = data;
+				switch (ITEM_TYPE[payload.type]) {
+					case "INCOME":
+						if (payload.debit < 0) {
+							reject(message["noteitem.income_gt_0"]);
+						} else {
+							this.incomeTransaction(
+								payload.walletNoteId,
+								payload.debit
+							)
+								.then(async (result) => {
+									payload["credit"] = 0;
+									const savingData =
+										await new schema.NoteItemSchema(
+											payload
+										).save();
 
-				if (this.isCanAdd(note, data)) {
-					let payload = data;
-					switch (ITEM_TYPE[payload.type]) {
-						case "INCOME":
-							if (payload.debit < 0) {
-								reject(message["noteitem.income_gt_0"]);
-							} else {
-								this.incomeTransaction(
-									payload.walletNoteId,
-									payload.debit
-								)
-									.then(async (result) => {
-										payload["credit"] = 0;
-										const savingData =
-											await new schema.NoteItemSchema(
-												payload
-											).save();
+									resolve(savingData);
+								})
+								.catch((err) => reject(err));
+						}
+						break;
+					case "WITHDRAW_OR_TRANSFER":
+						if (payload.debit < 0) {
+							reject(message["noteitem.transfer_gt_0"]);
+						} else {
+							this.withdrawOrTransferTransaction(
+								payload.walletNoteId, //pengirim
+								payload.walletNoteId2, //penerima
+								payload.debit
+							)
+								.then(async (result) => {
+									const savingData =
+										await new schema.NoteItemSchema(
+											payload
+										).save();
+									resolve(savingData);
+								})
+								.catch((err) => reject(err));
+						}
+						break;
+					case "SPEND":
+						if (payload.credit < 0) {
+							reject(message["noteitem.spend_gt_0"]);
+						} else {
+							this.spendTransaction(
+								payload.walletNoteId,
+								payload.categoryNoteId,
+								payload.credit
+							)
+								.then(async (result) => {
+									payload["debit"] = 0;
+									const savingData =
+										await new schema.NoteItemSchema(
+											payload
+										).save();
 
-										resolve(savingData);
-									})
-									.catch((err) => reject(err));
-							}
-							break;
-						case "WITHDRAW_OR_TRANSFER":
-							if (payload.debit < 0) {
-								reject(message["noteitem.transfer_gt_0"]);
-							} else {
-								this.withdrawOrTransferTransaction(
-									payload.walletNoteId, //pengirim
-									payload.walletNoteId2, //penerima
-									payload.debit
-								)
-									.then(async (result) => {
-										const savingData =
-											await new schema.NoteItemSchema(
-												payload
-											).save();
-										resolve(savingData);
-									})
-									.catch((err) => reject(err));
-							}
-							break;
-						case "SPEND":
-							if (payload.credit < 0) {
-								reject(message["noteitem.spend_gt_0"]);
-							} else {
-								this.spendTransaction(
-									payload.walletNoteId,
-									payload.categoryNoteId,
-									payload.credit
-								)
-									.then(async (result) => {
-										payload["debit"] = 0;
-										const savingData =
-											await new schema.NoteItemSchema(
-												payload
-											).save();
+									resolve(savingData);
+								})
+								.catch((err) => reject(err));
+						}
+						break;
+					case "SPEND_ONLY_IN_WALLET":
+						if (payload.credit < 0) {
+							reject(message["noteitem.spend_gt_0"]);
+						} else {
+							this.spendOnlyInWalletTransaction(
+								payload.walletNoteId,
+								payload.credit
+							)
+								.then(async (result) => {
+									payload["debit"] = 0;
+									const savingData =
+										await new schema.NoteItemSchema(
+											payload
+										).save();
 
-										resolve(savingData);
-									})
-									.catch((err) => reject(err));
-							}
-							break;
-						case "SPEND_ONLY_IN_WALLET":
-							if (payload.credit < 0) {
-								reject(message["noteitem.spend_gt_0"]);
-							} else {
-								this.spendOnlyInWalletTransaction(
-									payload.walletNoteId,
-									payload.credit
-								)
-									.then(async (result) => {
-										payload["debit"] = 0;
-										const savingData =
-											await new schema.NoteItemSchema(
-												payload
-											).save();
+									resolve(savingData);
+								})
+								.catch((err) => reject(err));
+						}
 
-										resolve(savingData);
-									})
-									.catch((err) => reject(err));
-							}
-
-							break;
-						default:
-							reject(message["noteitem.invalid_type"]);
-							break;
-					}
-				} else {
-					reject(
-						message["noteitem.invalid_date"]
-						// `Cannot add item. The range of note is from 26-${
-						// 	noteMonth - 1 === 0 ? 12 : noteMonth - 1
-						// }-${
-						// 	noteMonth - 1 === 0 ? noteYear - 1 : noteYear
-						// } to 25-${noteMonth}-${noteYear}`
-					);
+						break;
+					default:
+						reject(message["noteitem.invalid_type"]);
+						break;
 				}
 			})
 			.catch((error) => reject(error));
@@ -311,103 +272,75 @@ exports.editFunction = (id, data) => {
 				noteModel
 					.getById(noteItem.noteId)
 					.then((note) => {
-						// const noteYear = new Date(note.date).getFullYear();
-						// const noteMonth = new Date(note.date).getMonth() + 1;
-						if (
-							(data.date && this.isCanAdd(note, data)) ||
-							data.date === undefined
-						) {
-							switch (ITEM_TYPE[noteItem.type]) {
-								case "INCOME":
-									if (data.debit < 0) {
-										reject(message["noteitem.income_gt_0"]);
-									} else {
-										this.incomeTransaction(
-											noteItem.walletNote._id,
-											-noteItem.debit + data.debit
-										)
-											.then((income) => {
-												this.edit(id, data)
-													.then((res) => resolve(res))
-													.catch((err) =>
-														reject(err)
-													);
-											})
-											.catch((err) => reject(err));
-									}
-									break;
-								case "WITHDRAW_OR_TRANSFER":
-									if (data.debit < 0 || data.credit < 0) {
-										reject(
-											message["noteitem.transfer_gt_0"]
-										);
-									} else {
-										this.withdrawOrTransferTransaction(
-											noteItem.walletNote._id,
-											noteItem.walletNote2._id,
-											-noteItem.debit + data.debit
-										)
-											.then((wott) => {
-												this.edit(id, data)
-													.then((res) => resolve(res))
-													.catch((err) =>
-														reject(err)
-													);
-											})
-											.catch((err) => reject(err));
-									}
+						switch (ITEM_TYPE[noteItem.type]) {
+							case "INCOME":
+								if (data.debit < 0) {
+									reject(message["noteitem.income_gt_0"]);
+								} else {
+									this.incomeTransaction(
+										noteItem.walletNote._id,
+										-noteItem.debit + data.debit
+									)
+										.then((income) => {
+											this.edit(id, data)
+												.then((res) => resolve(res))
+												.catch((err) => reject(err));
+										})
+										.catch((err) => reject(err));
+								}
+								break;
+							case "WITHDRAW_OR_TRANSFER":
+								if (data.debit < 0 || data.credit < 0) {
+									reject(message["noteitem.transfer_gt_0"]);
+								} else {
+									this.withdrawOrTransferTransaction(
+										noteItem.walletNote._id,
+										noteItem.walletNote2._id,
+										-noteItem.debit + data.debit
+									)
+										.then((wott) => {
+											this.edit(id, data)
+												.then((res) => resolve(res))
+												.catch((err) => reject(err));
+										})
+										.catch((err) => reject(err));
+								}
 
-									break;
-								case "SPEND":
-									if (data.credit < 0) {
-										reject(message["noteitem.spend_gt_0"]);
-									} else {
-										this.spendTransaction(
-											noteItem.walletNote._id,
-											noteItem.categoryNote._id,
-											-noteItem.credit + data.credit
-										)
-											.then((spend) => {
-												this.edit(id, data)
-													.then((res) => resolve(res))
-													.catch((err) =>
-														reject(err)
-													);
-											})
-											.catch((err) => reject(err));
-									}
+								break;
+							case "SPEND":
+								if (data.credit < 0) {
+									reject(message["noteitem.spend_gt_0"]);
+								} else {
+									this.spendTransaction(
+										noteItem.walletNote._id,
+										noteItem.categoryNote._id,
+										-noteItem.credit + data.credit
+									)
+										.then((spend) => {
+											this.edit(id, data)
+												.then((res) => resolve(res))
+												.catch((err) => reject(err));
+										})
+										.catch((err) => reject(err));
+								}
 
-									break;
-								case "SPEND_ONLY_IN_WALLET":
-									if (data.credit < 0) {
-										reject(message["noteitem.spend_gt_0"]);
-									} else {
-										this.spendOnlyInWalletTransaction(
-											noteItem.walletNote._id,
-											-noteItem.credit + data.credit
-										)
-											.then((soiw) => {
-												this.edit(id, data)
-													.then((res) => resolve(res))
-													.catch((err) =>
-														reject(err)
-													);
-											})
-											.catch((err) => reject(err));
-									}
-									break;
-							}
-						} else {
-							reject(
-								message["noteitem.invalid_date"]
-								// `Cannot add item. The range of note is from 26-${
-								// 	noteMonth - 1 === 0 ? 12 : noteMonth - 1
-								// }-${
-								// 	noteMonth - 1 === 0
-								// 		? noteYear - 1
-								// 		: noteYear
-								// } to 25-${noteMonth}-${noteYear}`
-							);
+								break;
+							case "SPEND_ONLY_IN_WALLET":
+								if (data.credit < 0) {
+									reject(message["noteitem.spend_gt_0"]);
+								} else {
+									this.spendOnlyInWalletTransaction(
+										noteItem.walletNote._id,
+										-noteItem.credit + data.credit
+									)
+										.then((soiw) => {
+											this.edit(id, data)
+												.then((res) => resolve(res))
+												.catch((err) => reject(err));
+										})
+										.catch((err) => reject(err));
+								}
+								break;
 						}
 					})
 					.catch((err) => reject(err));
